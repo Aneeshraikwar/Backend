2e4db90b601973945514f62b121f1c5a4a98dd6f
Aneeshraikwar <aneeshraikwar@gmail.com>
2024-09-10 20:47:19 +0530

    added changePassword route

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.js";
-import { LoginUser, RegisterUser, logOut,refreshingAccessToken } from "../controllers/User.js";
+import { LoginUser, RegisterUser, changePassword, logOut,refreshingAccessToken ,} from "../controllers/User.js";
 import { varifyJWT } from "../middlewares/Auth.js";
 const router = Router();
 /* --------------------------- RegisterUser route --------------------------- */
@@ -26,4 +26,5 @@ router.route("/login").post(LoginUser);
 router.route("/logout").post(varifyJWT, logOut);
 /* ------------------------- route for refresh token ------------------------ */
 router.route('/refreshToken').post(refreshingAccessToken)
+router.route("/changePassword").post(varifyJWT,changePassword)
 export default router;
