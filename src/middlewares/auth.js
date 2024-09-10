099ee8625348e438e31fc4d01e9470fb57d67bf1
Aneeshraikwar <aneeshraikwar@gmail.com>
2024-09-10 20:50:15 +0530

    fix bug in auth middlware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,24 +4,23 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 
  export const varifyJWT = asyncHandler(async (req, _, next) => {
-  console.log("hello 1");
+ 
   try {
-    console.log("inside try 2");
+  
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
-    console.log("after token", token);
+    
     if (!token) {
       console.log("inside if token 4");
       throw new ApiError(401, "Unauthorizad request || Token is not found  ");
     }
-    console.log("hello 5");
+    
     const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    console.log("hello 6");
-    console.log("decodedToken ", decodedToken);
+    
     const user = await User.findById(decodedToken?._id).select(
       "-password -accesToken"
     );
-    console.log("hello 7");
+   
     if (!user) {
       throw new ApiError(401, "Invalide accessToken  ");
     }
